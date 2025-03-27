653ed54c4008e0c7910bbb92507abeb76d18e5cb
Isaac Robinson <isaac.robsn@gmail.com>
2025-03-27 14:05:34 -0700

    print line

--- Animal.java
+++ Animal.java
@@ -2,16 +2,20 @@ public class Animal extends LivingThings {
     public Animal(int birthYear) {
         super(birthYear);
     }
+
     public int calculateAge() {
         return super.calculateAge();
     }
+
     public String consume(String food) {
         return "Eating " + food;
     }
+
     public int reproduce(int numOffspring) {
         return numOffspring;
     }
+
     public void makeSound() {
-        System.out.print("Animal sound");
+        System.out.println("Animal sound");
     }
 }
