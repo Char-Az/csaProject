c19228502376807fe702b647f8612ac10b4aceb3
陳 <noahchen482@gmail.com>
2025-03-27 13:57:12 -0700

    Update Human.java

--- Human.java
+++ Human.java
@@ -9,8 +9,12 @@ public class Human extends Animal {
     public String getRegion() {
         return region;
     }
+    public void name(String name) {
+        System.out.println("My name is " + name);
+    }
 
     public void dance() {
         System.out.println("I am from " + region + " and I am dancing.");
     }
+
 }
