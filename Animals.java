233cc7e8718ad884c06ddff92bac27bb9ac03917
陳 <noahchen482@gmail.com>
2025-03-27 13:44:46 -0700

    Update Animals.java

--- Animals.java
+++ Animals.java
@@ -2,4 +2,7 @@ public class Animals extends LivingThings {
     public Animals(int birthYear) {
         super(birthYear);
     }
+    public int calculateAge() {
+        return super.calculateAge();
+    }
 }
